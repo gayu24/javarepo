f65d384ae9aefc8573cc23cd4d7bb3b5f84749df
gayu24 <shindegr.95@gmail.com>
2024-10-31 11:07:48 +0530

    added new addition file

--- Addition.java
+++ Addition.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println(c );
+    }
 }
